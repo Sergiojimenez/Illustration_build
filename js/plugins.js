c25f5aacc5c544a5d7a206799c54479cec47e7e7
Sergio Jimenez Corominas <sergio@claimsoluciones.com>
2015-10-31 20:59:42 +0100

    First commit

---
+++ js/plugins.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+/*
+plugins settings
+*/
+      $('.slider').slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			adaptiveHeight: true,
+			fade: true,
+  			cssEase: 'linear'
+      });
+
+});
